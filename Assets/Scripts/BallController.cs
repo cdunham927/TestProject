76cb45bf80867926ecb80bb0c2edf7684d3db6bc
cdunham927 <cdunham927@gmail.com>
2021-01-28 18:29:45 -0800

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Rigidbody2D bod;
+    public float spd;
+
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        bod.AddForce(Vector2.up * spd);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Wall")) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float spd;
+    Rigidbody2D bod;
+    Vector2 input;
+    public GameObject ball;
+    public float maxHp;
+    [SerializeField]
+    float hp;
+    public Image healthBar;
+
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>();
+
+        hp = maxHp;
+    }
+
+    void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (input.x != 0)
+        {
+            bod.AddForce(Vector2.right * spd * input.x * Time.deltaTime);
+        }
+        if (input.y != 0)
+        {
+            bod.AddForce(Vector2.up * spd * input.y * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ball, transform.position, Quaternion.identity);
+        }
+
+        healthBar.fillAmount = (hp / maxHp);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            hp -= 15;
+        }
+    }
+}
